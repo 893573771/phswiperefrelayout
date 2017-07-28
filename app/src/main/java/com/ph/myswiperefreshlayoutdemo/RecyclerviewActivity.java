da69d55541c8d7ed5319449daa9761be02fe1a06
893573771 <893573771@qq.com>
2017-07-28 16:41:38 +0800

    添加recyclerview

--- app/src/main/java/com/ph/myswiperefreshlayoutdemo/RecyclerviewActivity.java
+++ app/src/main/java/com/ph/myswiperefreshlayoutdemo/RecyclerviewActivity.java
@@ -1,57 +1,42 @@
 package com.ph.myswiperefreshlayoutdemo;
 
-import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
+import com.ph.phswiperefreshlayout.PhRecyclerViewAdapter;
 import com.ph.phswiperefreshlayout.PhSwipeRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  * Created by ph on 2017/7/26.
  */
-public class MainActivity extends AppCompatActivity {
 
+public class RecyclerviewActivity extends AppCompatActivity{
     private PhSwipeRefreshLayout mRefreshLayout;
-    private ListView mListView;
+    private RecyclerView mListView;
+    LinearLayoutManager mLinearLayoutManager;
     private List<String> mDatas;
     private List<String> mNewDatas;
-    private ArrayAdapter<String> mAdapter;
+    private PhRecyclerViewAdapter<String> mAdapter;
     private int mNowPage = 1;
     private int mTotalPageNum = 3;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recyclerview);
         initView();
         initRefresh();
-        initRefreshDatas();
-        initClick();
-    }
-
-    private void initClick() {
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (view == mRefreshLayout.getFooterView()){
-                    Toast.makeText(MainActivity.this, "底部", Toast.LENGTH_SHORT).show();
-                }else if (position == 4){
-                    Log.i("hh", "跳转前");
-                    startActivity(new Intent(MainActivity.this, RecyclerviewActivity.class));
-                }else {
-                    mDatas.remove(position);
-                    mAdapter.notifyDataSetChanged();
-                }
-            }
-        });
+//        initRefreshDatas();
     }
 
     private void initRefresh() {
@@ -66,12 +51,15 @@ public class MainActivity extends AppCompatActivity {
                 initRefreshDatas();
             }
         });
+        mRefreshLayout.setRefreshing(true);
+        initRefreshDatas();
     }
 
     private void initLoadMoreDatas() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                Log.i("hh", "加载更多数据");
                 mDatas.addAll(getMoreData());
                 // 加载完数据设置为不加载状态，将加载进度收起来
 //                mSwipeRefreshView.setLoading(false);
@@ -92,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
                 mNowPage = 1;
                 mDatas.clear();
                 mDatas.addAll(getData());
+                mAdapter.setHasAddShowFooterView(false, false);
                 mAdapter.notifyDataSetChanged();
                 mRefreshLayout.setRefreshLoadMoreInfoFinish(PhSwipeRefreshLayout.REFRESH_FINISH, true);
             }
@@ -99,12 +88,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        mRefreshLayout = (PhSwipeRefreshLayout) findViewById(R.id.my_refresh_layout);
-        mListView = (ListView) findViewById(R.id.lv_data);
+        mRefreshLayout = (PhSwipeRefreshLayout) findViewById(R.id.ph_refreshlayout);
+        mListView = (RecyclerView) findViewById(R.id.rv_list);
+        mLinearLayoutManager = new LinearLayoutManager(this);
+        mLinearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         mDatas = new ArrayList<>();
-        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mDatas);
+        mAdapter = new PhRecyclerViewAdapter<String>(this, R.layout.item_recyclerview, mDatas) {
+            @Override
+            protected void setClickListener(ViewHolder holder, final int position) {
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mDatas.remove(position);
+                        mAdapter.setOneItemRemoved();
+                        mAdapter.notifyDataSetChanged();
+                    }
+                });
+                holder.getView(R.id.tv_rv).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(RecyclerviewActivity.this, "点击了textview" + position, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void convert(PhRecyclerViewAdapter.ViewHolder holder, String s) {
+                holder.setText(R.id.tv_rv, s);
+            }
+        };
+        mListView.setLayoutManager(mLinearLayoutManager);
         mListView.setAdapter(mAdapter);
-        mRefreshLayout.setRefreshing(true);
     }
 
 
